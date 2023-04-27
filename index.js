6dd28d03d91a45323d181ad506bb72b1d211a3c8
Aryaman Srivastava <git.y56pp@slmail.me>
2023-04-27 23:52:26 +0530

    add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.price-box');
+const weatherDetails = document.querySelector('.change');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+
+    
